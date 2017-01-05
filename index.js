88713d7c0bcb345d15a34dc0ae3ba91871892fc2
slashnot <slashnot84@gmail.com>
2017-01-05 20:33:14 +0530

    Heroku Dev

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-// server.js
-var jsonServer = require('json-server')
-var server = jsonServer.create()
-var router = jsonServer.router('data.json')
-var middlewares = jsonServer.defaults()
+var express = require('express');
+var app = express();
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, function () {
-  console.log('JSON Server is running');
-})
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+//
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
